43f8ae251d92766e174b9537824fd44efb823434
sukritirawal1 <rawalsukriti@gmail.com>
2025-03-15 01:07:46 -0400

    Document Processing updates: removes empty sections, titles, and urls. Changed semantic chunking to section/heading based

--- knowledge_base/DocumentProcessor.py
+++ knowledge_base/DocumentProcessor.py
@@ -4,30 +4,27 @@ import html
 import unicodedata
 import re
 import numpy as np
-from langchain_experimental.text_splitter import SemanticChunker
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_core.documents import Document
+# from langchain_experimental.text_splitter import SemanticChunker
+# from langchain_community.embeddings import HuggingFaceEmbeddings
+# from langchain_core.documents import Document
 
 
 class DocumentProcessor:
-    def __init__(self, ROOT = os.getcwd(), input_dir="json_documents", output_dir="processed_documents", min_paragraph_length=200, chunk_size=2000, chunk_overlap=200):
+    def __init__(self, ROOT = os.getcwd(), input_dir="json_documents", output_dir="processed_documents", min_paragraph_length=200, chunk_size=5000):
         self.input_dir = os.path.join(ROOT, input_dir)
         self.output_dir = os.path.join(ROOT, output_dir)
         self.chunk_size = chunk_size
         self.min_paragraph_length = min_paragraph_length
-        self.embeddings = HuggingFaceEmbeddings(
-            model_name="all-MiniLM-L6-v2",
-            model_kwargs={'device': 'cpu'},
-            encode_kwargs={'normalize_embeddings': True}
-        )
-        self.chunk_overlap = chunk_overlap
-        self.chunker = SemanticChunker(
-            embeddings=self.embeddings,
-            buffer_size=2,  # Consider 2 sentences for context
-            threshold=0.5,   # Higher threshold = more chunks
-            chunk_size=self.chunk_size,
-            chunk_overlap=self.chunk_overlap
-        )
+        # self.embeddings = HuggingFaceEmbeddings(
+        #     model_name="all-MiniLM-L6-v2",
+        #     model_kwargs={'device': 'cpu'},
+        #     encode_kwargs={'normalize_embeddings': True}
+        # )
+        # self.chunker = SemanticChunker(
+        #     embeddings=self.embeddings,
+        #     buffer_size=2,  # Consider 2 sentences for context
+        #     breakpoint_threshold_amount=0.5   # Higher threshold = more chunks
+        # )
         
         os.makedirs(output_dir, exist_ok=True)
         
@@ -47,6 +44,9 @@ class DocumentProcessor:
         content = re.sub(r' +', ' ', content)
         content = re.sub(r'[\x00-\x08\x0B\x0C\x0E-\x1F\x7F-\x9F]', '', content)
         
+        content = re.sub(r'https?://\S+', '', content)
+        content = re.sub(r'www\.\S+', '', content)
+        
         lines = [line.strip() for line in content.split('\n')]
         lines = [line for line in lines if line]
         content = '\n'.join(lines)
@@ -54,124 +54,115 @@ class DocumentProcessor:
     
         return content
     
-    def merge_paragraphs(self, cleaned_text):
-        heading_pattern = re.compile(r'^(.*)\n-+$', re.MULTILINE)
-        paragraphs = re.split(r'\n\n+', cleaned_text)
+    #semantic implementaiton - it's not working as well as using headers    
+    # def chunk_doc(self, doc):
+    #     content = doc['content']
+    #     metadata = doc['metadata']
         
-        merged_paragraphs = []
-        i = 0
         
-        while i < len(paragraphs):
-            current = paragraphs[i].strip()
-            is_heading = heading_pattern.search(current) is not None
-            
-            if is_heading:
-                merged_paragraphs.append(current)
-                i += 1
-                continue
-                
-            merged = current
-            i += 1
-            
-            while (i < len(paragraphs) and 
-                len(merged) < self.min_paragraph_length and 
-                not heading_pattern.search(paragraphs[i].strip())):
-                
-                next_para = paragraphs[i].strip()
-                merged += "\n\n" + next_para
-                i += 1
-                
-            if merged:
-                merged_paragraphs.append(merged)
+    #     clean_content = self.clean_text(content)
+    #     clean_content = self.merge_paragraphs(clean_content)
         
-        return "\n\n".join(merged_paragraphs)
-    
-    def calculate_similarity(self, vec1, vec2):
-        dot_product = np.dot(vec1, vec2)
-        norm1 = np.linalg.norm(vec1)
-        norm2 = np.linalg.norm(vec2)
+    #     doc = Document(page_content=clean_content, metadata = metadata)
         
-        if norm1 == 0 or norm2 == 0:
-            return 0
-            
-        return dot_product / (norm1 * norm2)
-    
+    #     split_doc = self.chunker.split_documents([doc])
+        
+    #     chunks = []
+    #     for i, chunk in enumerate(split_doc):
+    #         chunk.metadata["chunk_id"] = i
+    #         chunks.append({"page_content": chunk.page_content,
+    #                        "metadata": chunk.metadata})
+    #     return chunks
+        
+        
     def chunk_doc(self, doc):
         content = doc['content']
         metadata = doc['metadata']
         
+        cleaned_text = self.clean_text(content)
         
-        clean_content = self.clean_text(content)
-        clean_content = self.merge_paragraphs(clean_content)
-        
-        doc = Document(page_content=clean_content, metadata = metadata)
-        
-        split_doc = self.chunker.split_documents([doc])
-        
+        lines = cleaned_text.split('\n')[2:]
         chunks = []
-        for i, chunk in enumerate(split_doc):
-            chunk.metadata["chunk_id"] = i
-            chunks.append({"page_content": chunk.page_content,
-                           "metadata": chunk.metadata})
-        return chunks
+        i = 0
+        skip_section = False
         
-        
-    # def chunk_doc(self, doc):
-    #     content = doc['content']
-    #     metadata = doc['metadata']
-    #     logical_sections = re.split(r'\n([^\n]+)\n[-]+\n', content)
-    #     #break into logical chunks which do not exceed chunK_size
-    #     chunks = []
-    #     for section in logical_sections:
-    #         #check if section fits in chunk. if not, break into paragraphs
-    #         current_text = ""
-    #         if len(section) > self.chunk_size:
-    #             paragraphs = re.split(r'\n\n+', section)
-    #             current_text = ""
-    #             current_length = 0
-                
-    #             for p in paragraphs:
-    #                 #if paragraph doesn't fit in chunk -> finalize chunk
-    #                 if current_length + len(p) > self.chunk_size and current_text != "":
-    #                     chunks.append({"page_content": current_text,
-    #                                    "metadata": {**metadata, "chunk_id": len(chunks)}})
-                        
-    #                     #reset current check based on if chunk overlap is desired
-    #                     if self.chunk_overlap > 0:
-    #                         overlap_start = max(0, len(current_text) - self.chunk_overlap)
-    #                         overlap_text = current_text[overlap_start:]
-    #                         last_p = overlap_text.rfind('\n\n') #find last paragraph break
-    #                         if last_p != -1: current_text = overlap_text[last_p + 2:]
-    #                         else: current_text = overlap_text
-    #                     else:
-    #                         current_text = ""
-                            
-    #                 #start current_texta s p if it's empty      
-    #                 if current_text == "": current_text = p
-    #                 else: current_text += "\n\n" + p
-    #         else:
-    #             chunks.append({"page_content": section,
-    #                             "metadata": {**metadata, "chunk_id": len(chunks)}})
-    #         #last chunk
-    #         if current_text != "":
-    #             chunk_doc = {
-    #                 "page_content": current_text,
-    #                 "metadata": {
-    #                     **metadata,
-    #                     "chunk_id": len(chunks)
-    #                 }
-    #             }
-    #             chunks.append(chunk_doc)
-        
-    #     # chunk_count
-    #     for chunk in chunks:
-    #         chunk["metadata"]["chunk_count"] = len(chunks)
+        while i < len(lines):
+            line = lines[i].strip()
             
-    #     return chunks
-                        
+            #check for header line
+            if i + 1 < len(lines) and re.match(r'^-+$', lines[i+1].strip()):
+                skip_section = line.lower() in ["contents", "see also", "references", "external links"]
+                if skip_section: #skip unecessary sections
+                    i += 2  #skip two lines
+                    while i < len(lines) and not (i + 1 < len(lines) and re.match(r'^-+$', lines[i+1].strip())): #iterate through to next header
+                        i += 1
+                    continue
+                
+                header = line
+                i += 2  #skip 2 lines
+                
+                section_content = []
+                while i < len(lines) and not (i + 1 < len(lines) and re.match(r'^-+$', lines[i+1].strip())): #collect content until next header
+                    section_content.append(lines[i])
+                    i += 1
+                
+                #skip empty
+                if not section_content:
+                    continue
+                
+                #attach to header
+                section_text = header + "\n" + "\n".join(section_content)
+                if len(section_text) <= self.chunk_size: #check if it fits
+                    chunks.append({
+                        "page_content": section_text,
+                        "metadata": {**metadata, "chunk_id": len(chunks)}
+                    })
+                else:
+                    # split into paragraphs
+                    paragraphs = re.split(r'\n\n+', "\n".join(section_content))
+                    current_chunk = header
                     
+                    for para in paragraphs:
+                        if len(current_chunk) + len(para)> self.chunk_size and current_chunk != header:
+                            chunks.append({
+                                "page_content": current_chunk,
+                                "metadata": {**metadata, "chunk_id": len(chunks)}
+                            })
+                            current_chunk = header + "\n" + para
+                        else:
+                            current_chunk += "\n\n" + para
+                    
+                    # add in any trailing content
+                    if current_chunk and current_chunk != header:
+                        chunks.append({
+                            "page_content": current_chunk,
+                            "metadata": {**metadata, "chunk_id": len(chunks)}
+                        })
+            else:#check for any content before header
+                if not skip_section:
+                    pre_header_content = []
+                    while i < len(lines) and not (i + 1 < len(lines) and re.match(r'^-+$', lines[i+1].strip())):
+                        if lines[i].strip():
+                            pre_header_content.append(lines[i].strip())
+                        i += 1
+                    
+                    if pre_header_content:
+                        content_text = "\n".join(pre_header_content)
+                        chunks.append({
+                            "page_content": content_text,
+                            "metadata": {**metadata, "chunk_id": len(chunks)}
+                        })
+                else:
+                    i += 1
+        
+        for chunk in chunks:
+            chunk["metadata"]["chunk_count"] = len(chunks)
+        
+        return chunks
+                                
+                        
     def process_and_save_docs(self, output_filename="langchain_documents.json"):
-        docs = self.load_docs()[:10]
+        docs = self.load_docs()
         all_chunks = []
         for doc in docs:
             chunks = self.chunk_doc(doc)
@@ -188,6 +179,7 @@ def main():
     processor = DocumentProcessor()
     #saves docs to output_directory/output_filename, returns list of page_content/metadata dicts
     processed_docs = processor.process_and_save_docs(output_filename="langchain_documents.json") 
+    print(len(processed_docs))
     
 if __name__== "__main__":
     main()
